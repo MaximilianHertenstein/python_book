bf219396a23a27fc3d7e8625ff4dafd9ce4546e4
MaximilianHertenstein <hert@ghse.de>
2026-02-01 08:47:01 +0100

    css

--- src/book_theme/thebe_init.js
+++ src/book_theme/thebe_init.js
@@ -1,7 +1,4 @@
 (() => {
-
-
-
   addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('pre code.language-python').forEach(c => 
       c.parentElement?.setAttribute('data-executable', 'true')
