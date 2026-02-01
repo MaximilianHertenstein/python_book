37c69097aaa11187fa30ebf8445ab5ce335a0353
MaximilianHertenstein <hert@ghse.de>
2026-02-01 12:18:02 +0100

    simolify

--- src/book_theme/thebe_init.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('pre code.language-python').forEach(c => 
-      c.parentElement?.setAttribute('data-executable', 'true')
-    );
-  });
-})();
